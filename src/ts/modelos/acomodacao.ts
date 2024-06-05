732cc0f90e7f9c4abf346bff0cc2b1bc74536ae4
Katiane <katiane.soares@fatec.sp.gov.br>
2024-06-05 10:59:00 -0300

    ajustes nos atributos da classe acomodacao

--- src/ts/modelos/acomodacao.ts
+++ src/ts/modelos/acomodacao.ts
@@ -1,28 +1,45 @@
-import { NomeAcomadacao } from "../enumeracoes/NomeAcomadacao"
+import { NomeAcomadacao } from "../enumeracoes/NomeAcomadacao";
 
 export default class Acomodacao {
-    private nomeAcomadacao: NomeAcomadacao
-    private camaSolteiro: Number
-    private camaCasal: Number
-    private suite: Number
-    private climatizacao: Boolean
-    private garagem: Number
+  private nomeAcomadacao: NomeAcomadacao;
+  private camaSolteiro: number;
+  private camaCasal: number;
+  private suite: number;
+  private climatizacao: boolean;
+  private garagem: number;
 
-    constructor(nomeAcomadacao: NomeAcomadacao, camaSolteiro: Number, camaCasal: Number,
-        suite: Number, climatizacao: Boolean, garagem: Number) {
-        this.nomeAcomadacao = nomeAcomadacao
-        this.camaSolteiro = camaSolteiro
-        this.camaCasal = camaCasal
-        this.suite = suite
-        this.climatizacao = climatizacao
-        this.garagem = garagem
-    }
+  constructor(
+    nomeAcomadacao: NomeAcomadacao,
+    camaSolteiro: number,
+    camaCasal: number,
+    suite: number,
+    climatizacao: boolean,
+    garagem: number
+  ) {
+    this.nomeAcomadacao = nomeAcomadacao;
+    this.camaSolteiro = camaSolteiro;
+    this.camaCasal = camaCasal;
+    this.suite = suite;
+    this.climatizacao = climatizacao;
+    this.garagem = garagem;
+  }
 
-    public get NomeAcomadacao() { return this.nomeAcomadacao }
-    public get CamaSolteiro() { return this.camaSolteiro }
-    public get CamaCasal() { return this.camaCasal }
-    public get Suite() { return this.suite }
-    public get Climatizacao() { return this.climatizacao }
-    public get Garagem() { return this.garagem }
-
+  public get NomeAcomadacao() {
+    return this.nomeAcomadacao;
+  }
+  public get CamaSolteiro() {
+    return this.camaSolteiro;
+  }
+  public get CamaCasal() {
+    return this.camaCasal;
+  }
+  public get Suite() {
+    return this.suite;
+  }
+  public get Climatizacao() {
+    return this.climatizacao;
+  }
+  public get Garagem() {
+    return this.garagem;
+  }
 }
